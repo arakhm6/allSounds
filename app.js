428b0783442a2238625da93969035f40a0b4579c
arakhm6 <arakhm6@gmail.com>
2021-02-18 22:16:25 -0500

    sound loop

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ class DrumKit
     {
         this.playButton = document.querySelector('.play');
         this.pads = document.querySelectorAll('.pad');
-        this.kickAudion = document.querySelector('.kick-sound');
-        this.snareAudion = document.querySelector('.snare-sound');
-        this.hihatAudion = document.querySelector('.hihat-sound');
+        this.kickAudio = document.querySelector('.kick-sound');
+        this.snareAudio = document.querySelector('.snare-sound');
+        this.hihatAudio = document.querySelector('.hihat-sound');
         this.index = 0;
         this.bpm = 150;
     }
@@ -20,6 +20,28 @@ class DrumKit
     {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
+        activeBars.forEach(bar =>
+        {
+            bar.style.animation = `playTrack 1s alternate ease-in-out 2`;
+            if(bar.classList.contains('active'))
+            {
+                if(bar.classList.contains('kick-pad'))
+                {
+                   //this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains('snare-pad'))
+                {
+                    //this.kickAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad'))
+                {
+                    //this.kickAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         this.index++; 
     }
 
@@ -39,6 +61,10 @@ const drumKit = new DrumKit();
 drumKit.pads.forEach(pad => 
     {
         pad.addEventListener('click', drumKit.activePad);
+        pad.addEventListener('animationend', function() 
+        {
+            this.style.animation = '';
+        });
     })
 drumKit.playButton.addEventListener('click', () => 
 {
